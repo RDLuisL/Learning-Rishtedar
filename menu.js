47711de2203286dc582f193ba73b3865181326d9
RDLuisL <dev.eng.luisluna@gmail.com>
2025-05-08 21:20:05 -0400

    version 3.0

--- menu.js
+++ menu.js
@@ -5,7 +5,7 @@ export const preguntas = [
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/samosas_1740159726Zl4FF8.jpg"
   },
   {
-    pregunta: "Achari Bengan 🍆",
+    pregunta: "Achari Bengan 🍆🥭",
     respuesta: "Berenjenas en encurtido de mango",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/achari_bengan_17401597269t4O9W.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvYWNoYXJpX2Jlbmdhbl8xNzQwMTU5NzI2OXQ0TzlXLmpwZyJ9.00Wp9HIV2-8NoVIRMl_MKOhcTOatxys6bHtjab8l1d8"
   },
@@ -20,17 +20,17 @@ export const preguntas = [
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/arroz_basmati_1740159730lDLkcD.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvYXJyb3pfYmFzbWF0aV8xNzQwMTU5NzMwbERMa2NELmpwZyJ9.vqjj-IoJ3bFyjCyAbbXAntpRp7ZX1kK2-y35gh8N17o"
   },
   {
-    pregunta: "❤️ Butter Chicken 🍗🧈",
+    pregunta: "❤️ Butter Chicken 🐓🧈",
     respuesta: "Pollo a la mantequilla",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/butter_chicken_1740159726XL0NWj.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvYnV0dGVyX2NoaWNrZW5fMTc0MDE1OTcyNlhMME5Xai5qcGcifQ.lAxUCGR-jLFWYWh3BCkZ4BDisCljHGPBTzN9hnlQGjA"
   },
   {
-    pregunta: "Dal Makhan 🧈",
+    pregunta: "Dal Makhan 🗑️🪣 🧈",
     respuesta: "Lentejas en mantequilla y especias",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/dal_makhani_1740159727xXF1VT.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvZGFsX21ha2hhbmlfMTc0MDE1OTcyN3hYRjFWVC5qcGcifQ.gOOIje2M71jreYTMKjwsXHwj9ldMnhdWfupHuhjfpyQ"
   },
   {
-    pregunta: "Dal Tadka 🍛",
+    pregunta: "Dal Tadka 🥣🍛",
     respuesta: "Lentejas rojas al curry",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/dal_tadka_1740159727ruFTs8.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvZGFsX3RhZGthXzE3NDAxNTk3MjdydUZUczguanBnIn0.gZwGI_Y_3PI-jLGwntUvmWoyGcIgd5XkHUkaCncx20o"
   },
@@ -45,7 +45,7 @@ export const preguntas = [
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/11-1.jpg"
   },
   {
-    pregunta: "Jheenga Til Tikka 🦐",
+    pregunta: "Jheenga Til Tikka 🍤🌱",
     respuesta: "Camarones apanados en sésamo",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/13.jpg"
   },
@@ -55,12 +55,12 @@ export const preguntas = [
     imagen: "https://citymagazine.cl/wp-content/uploads/2022/06/Angoori-Khumb-Aur-Matar-min-2.jpg"
   },
   {
-    pregunta: "Aloo matar aur gobhi 🥔🥦🧅🍅",
+    pregunta: "Aloo matar aur gobhi 🥣🥔🥦🧅🍅",
     respuesta: "Papas con arveja y coliflor en massala, condimentadas al estilo tradicional hindu con salsa de cebolla y tomate",
     imagen: "https://www.cookclickndevour.com/wp-content/uploads/2017/11/aloo-gobi-mutter-recipe-a.jpg"
   },
   {
-    pregunta: "Kadai Paneer Tikka Masal 🧀",
+    pregunta: "Kadai Paneer Tikka Masal 🥣🧀",
     respuesta: "Queso paneer en salsa masala",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/kadai_paneer_tikka_masala_1740159728CVuoNM.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMva2FkYWlfcGFuZWVyX3Rpa2thX21hc2FsYV8xNzQwMTU5NzI4Q1Z1b05NLmpwZyJ9.jTwJYgXmYG7TnRtiMgr-y93vg5syfjf0r8FTcdsz9vM"
   },
@@ -75,22 +75,22 @@ export const preguntas = [
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/malai_kofta_1740159728H2kC1n.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvbWFsYWlfa29mdGFfMTc0MDE1OTcyOEgya0Mxbi5qcGcifQ._VryQlSuwxWRecdE5Iyiwifxe5wbE5QwAMjUneQjl_U"
   },
   {
-    pregunta: "Mattar Paneer 🧀",
+    pregunta: "Mattar Paneer 🥣🧀",
     respuesta: "Queso paneer con arvejas",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/mattar_paneer_1742055351JLsVaB.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvbWF0dGFyX3BhbmVlcl8xNzQyMDU1MzUxSkxzVmFCLmpwZyJ9.leLrAiQdUYc1-oiH-5BZwqMvMD8RkxSnCcJ_RGwsIgY"
   },
   {
-    pregunta: "Murgh Tikka 🍗",
+    pregunta: "Murgh Tikka 🔥🐓",
     respuesta: "Pollo marinado al horno tandoor",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/murgh-tikka.jpg"
   },
   {
-    pregunta: "❤️ Murgh Curry 🍗",
+    pregunta: "❤️ Murgh Curry 🥣🐓",
     respuesta: "Pollo al curry tradicional",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/murgh_curry_1740159729b5zFyD.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvbXVyZ2hfY3VycnlfMTc0MDE1OTcyOWI1ekZ5RC5qcGcifQ.oSC6REdGUKOikkWJJsFWidFlYhhNPB5nbE75-d82eGA"
   },
   {
-    pregunta: "❤️ Pacora Mix",
+    pregunta: "❤️ Pacora Mix 🧀",
     respuesta: "Frituras de queso y verduras",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/pacora_mix_1741907534CzLQaS.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvcGFjb3JhX21peF8xNzQxOTA3NTM0Q3pMUWFTLmpwZyJ9.-fsJRVgkBbuV9TLHtKbGzCMI9h-xXheoCmmk-6GFjxU"
   },
@@ -100,52 +100,52 @@ export const preguntas = [
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/paneer_tikka_174015972862hBuz.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvcGFuZWVyX3Rpa2thXzE3NDAxNTk3Mjg2MmhCdXouanBnIn0.exkPR_ZVgUwM91nrB0NEKvnhj2QwnNrIs713A6S3RK8"
   },
   {
-    pregunta: "Goa Jheenga Curry 🦐",
+    pregunta: "Goa Jheenga Curry 🏖️🍤",
     respuesta: "Camarones al curry de Goa",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/goa_jheenga_curry_17401597300WySlB.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvZ29hX2poZWVuZ2FfY3VycnlfMTc0MDE1OTczMDBXeVNsQi5qcGcifQ.9ZfLkFYvMEkr0dyB8rGxYCbWK6O66n6tdQUpEu09MbA"
   },
   {
-    pregunta: "Achari Murgh Tikka Masala 🔥🏜️🍗🌶️🍋",
+    pregunta: "Achari Murgh Tikka Masala 🔥🏜️🐓🌶️🍋",
     respuesta: "Pollo ácido picante al tandoor",
     imagen: "https://cdn.mer-cat.com/rishtedar/400x400/img/products/achari_murgh_tikka_masala_1740159729HM47hW.jpg?pass=eyJhbGciOiJIUzI1NiJ9.eyJkYXRhIjoiNDAweDQwMC9pbWcvcHJvZHVjdHMvYWNoYXJpX211cmdoX3Rpa2thX21hc2FsYV8xNzQwMTU5NzI5SE00N2hXLmpwZyJ9.7yJEuLI8mpLXbW8qT6WOikqDX7mhZPTEOVabnscWFNg"
   },
   {
-    pregunta: "Chana Pindi Batura 🍛🧅",
+    pregunta: "Chana Pindi Batura 🥣🍛🧅",
     respuesta: "Garbanzo en masala servido con cebolla al puro estilo street food con bhatura",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/4.jpg"
   },
   {
     pregunta: "Bindi",
     respuesta: "Tercer Ojo Rojo: Casadas y Negro: Solteras",
-    imagen: "https://es.wikipedia.org/wiki/Archivo:Indian_Woman_with_bindi.jpg"
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Indian_Woman_with_bindi.jpg/500px-Indian_Woman_with_bindi.jpg"
   },
   {
-    pregunta: "Gulab Jamun",
+    pregunta: "Gulab Jamun 🧁x3",
     respuesta: "Bolitas de masa fritas remojadas en un jarabe dulce y ligeramente perfumado",
     imagen: "https://falasteenifoodie.com/wp-content/uploads/2024/11/DSC02391.jpg"
   },
   {
-    pregunta: "Bindi Oysters 🦪",
+    pregunta: "Bindi Oysters 🦪x6",
     respuesta: "Ostras",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/11/bhindi-oysters-768x768.jpg"
   },
   {
-    pregunta: "Chicken Lollipop 65 🍗",
+    pregunta: "Chicken Lollipop 65 🐓🍗x7",
     respuesta: "Muslitos fritos",
     imagen: "https://www.yummytummyaarthi.com/wp-content/uploads/2021/11/chicken-lollipop-1.jpeg"
   },
   {
-    pregunta: "Empanadas de Butter chicken 🥟🍗",
+    pregunta: "Empanadas de Butter chicken 🥟🐓",
     respuesta: "Empanadas de Pollo",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/12/empanada-butter-chicken.jpg"
   },
   {
-    pregunta: "Goa Chilly Bengan 🍆",
+    pregunta: "Goa Chilly Bengan 🏖️🥗🍆🧅🌶️🍅🌿",
     respuesta: "Berenjenas en salsa de cebolla, pimiento, tomate y especias.",
     imagen: "https://scontent.fscl8-1.fna.fbcdn.net/v/t39.30808-6/465692616_8766789766720445_1701687922643298364_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_ohc=UVS2ZtPQrCIQ7kNvwEgxeBm&_nc_oc=Adnx43piY2LLYFiDOFth5EhIKeVfmxfigEdKcRjcRFDLa1cp7MuW1-_ROvZTE9yBy20&_nc_zt=23&_nc_ht=scontent.fscl8-1.fna&_nc_gid=o7lWLaPhmLNNxJKfKihS3Q&oh=00_AfFZQXguajwJpFMBAiqv13k3jtzfnQ9KuazzMIsmuVvOdg&oe=6816CAC6"
   },
   {
-    pregunta: "Goa Chilly Jheenga 🦐",
+    pregunta: "Goa Chilly Jheenga 🏖️🥗🍤",
     respuesta: "Frescos camarones ecuatorianos en salsa de cebolla, pimiento, tomate y especias.",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/7.jpg"
   },
@@ -160,97 +160,97 @@ export const preguntas = [
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/11/machi-koliwada-1.jpg"
   },
   {
-    pregunta: "Veshnu Masala 🥦🍄🥔🍛🥕🌽",
+    pregunta: "Veshnu Masala 🥣🥦🍄🥔🍛🥕🌽",
     respuesta: "Guiso de verduras (coliflor, papa, champiñon, arveja, zanahoria, choclo) en cremosa salsa de cebolla y tomate",
     imagen: ""
   },
   {
-    pregunta: "Octupus Tikka Masala 🦑🧅",
+    pregunta: "Octupus Tikka Masala 🐙🧅",
     respuesta: "Pulpo en salsa Tikka Masala, con cebolla y pimientos al dente acompañados de especias.",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/11/octopus-tikka-masala2.jpg"
   },
   {
-    pregunta: "Tandoori Lamb Chop 🐑",
+    pregunta: "Tandoori Lamb Chop 🔥🐑🌿",
     respuesta: "Costilla de cordero , cocinada en tandoor con finas especias indias.",
     imagen: "https://i.pinimg.com/736x/37/fa/75/37fa75b98bb31417c3e224f23f9fe1a3.jpg"
   },
   {
-    pregunta: "Tandoori Mix",
+    pregunta: "Tandoori Mix 🍡",
     respuesta: "Mezcla de kabbabs",
     imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/taandoriM.jpg"
   },
 {
-  pregunta: "Mathan Chop Kabbab 🐑🍖",
+  pregunta: "Mathan Chop Kabbab 🐑🍖🔥🌿",
   respuesta: "Costilla de cordero , cocinada en tandoor con finas especias indias.",
   imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs2zpftjqU8tStom8Wm0--H2GxEPJVx1cfTQ&s"
 },
 {
-  pregunta: "Murgh Reshmi Kabab 🍗",
+  pregunta: "Murgh Reshmi Kabab 🐓🔥🍡",
   respuesta: "Pollo en salsa Reshmi (crema, cúrcuma, azafrán) cocinada al tandoor.",
   imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFOOGuq4smeYREjF_Op0Lx4romt56_AsutQg&s"
 },
 {
-  pregunta: "Murgh Tandoori 🍗",
+  pregunta: "Murgh Tandoori 🐓🔥",
   respuesta: "Truto de pollo con salsa Tikka al tandoor.",
   imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBoBdX3AbfY9bwpY9U406XJ3K_AuBFhDP_Mg&s"
 },
 {
-  pregunta: "Murgh Kaliu Mitch 🍗🧂🌰",
+  pregunta: "Murgh Kaliu Mitch 🥣🐓🧂🌰",
   respuesta: "Pollo en salsa blanca con castañas de caju y pimienta",
   imagen: "https://pipingpotcurry.com/wp-content/uploads/2023/03/Chicken-Kali-Mirch-Recipe-Piping-Pot-Curry.jpg"
 },
 {
-  pregunta: "❤️ Murgh Nilgiri 🍗🥥⛰️",
+  pregunta: "❤️ Murgh Nilgiri 🥣🐓🥥🍃⛰️",
   respuesta: "Pollo estilo sur de la india (montañas Nilgiri) coco, menta y cilantro cremoso",
   imagen: "https://bollywood-muenchen.com/wp-content/uploads/2023/10/18.jpg"
 },
 {
-  pregunta: "Murgh Tawa Masala 🍗🌶️🧅🍋",
+  pregunta: "Murgh Tawa Masala 🥣🐓🌶️🧅🍋",
   respuesta: "Pollo pimenton, cebolla y masala, salsa paprika y achari (acido picante)",
   imagen: "https://lh5.googleusercontent.com/proxy/DglZfkcJXwALGwP6ayU4LyQrsgyezYQfcnmEMv6FCT1MUfC--36DimaEmODYgbG7SE7wQjrGSnnErM88Zri7AAb-rB7pLxsyTQuMfNsKt0fOOSJxNWbWxrLOr-YOHOQ_0M3a8hi1v0uZ"
 },
 {
-  pregunta: "Murgh Mitha Suthra 🍗🥥",
+  pregunta: "Murgh Mitha Suthra 🥣🐓🥥",
   respuesta: "Pollo macerado en deliciosa crema de almendra, coco y masala sabor dulce.",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/murgh_mithaS.jpg"
 },
 {
-  pregunta: "❤️ Karay Murgh Tikka Masala 🍗🥥",
+  pregunta: "❤️ Karay Murgh Tikka Masala 🐓🥥",
   respuesta: "Especiado pollo en salsa tikka con masala y pimentón.",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/murgh_mithaS.jpg"
 },
 {
-  pregunta: "❤️ Lasuni Saag Murgh 🍗🧄🥬",
+  pregunta: "❤️ Lasuni Saag Murgh 🥣🐓🧄🥬",
   respuesta: "Pollo en salsa de espinacas con ajo cocinada al estilo indio con mucho masala",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/saagMurgh2.jpg"
 },
 {
-  pregunta: "Murgh Adraki 🍗🧅",
+  pregunta: "Murgh Adraki 🥣🐓🧅",
   respuesta: "Pollo salsa de cebolla con jengibre rallado",
   imagen: "https://i0.wp.com/www.omemade.com/wp-content/uploads/2022/10/Adraki-chicken-2.jpg?fit=1134%2C1606&ssl=1"
 },
 {
-  pregunta: "Murgh Punjabi 🏜️🍗🍅🧅🌿",
+  pregunta: "Murgh Punjabi 🏜️🐓🍅🧅🌿",
   respuesta: "Pollo condimentado estilo indio salsa tomate, cebolla, masala y aliño kasturi methi",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/Murg-4.jpg"
 },
 {
-  pregunta: "Murgh Tikka Pahadi 🍗🍢",
+  pregunta: "Murgh Tikka Pahadi 🔥🐓🍢",
   respuesta: "Especiado pollo cocinado al tandoor con bastante cilantro, sabor ahumado.",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/Murg-4.jpg"
 },
 {
   pregunta: "Chapati Kekrey 🦀🍅🧅🌿",
   respuesta: "Jaiba salsa de tomate con frito de cebolla y especies indias",
-  imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/10.jpg"
+  imagen: ""
 },
 {
-  pregunta: "Goa Machi Curry 🏖️🐟🧅🧄🥥",
+  pregunta: "Goa Machi Curry 🥣🏖️🐟🧅🧄🥥",
   respuesta: "Pescado salsa curry cebolla ajo coco y especias",
   imagen: "https://www.recipetineats.com/tachyon/2020/10/Goan-Fish-Curry_5.jpg"
 },
 {
-  pregunta: "Jheenga Masaledar 🦐🍅🧅",
+  pregunta: "Jheenga Masaledar 💏🥣🦐🍅🧅",
   respuesta: "Afrodisiaco camarones condimentados estilo indio salsa de tomate cebolla y masala aliño kasturi methi",
   imagen: "https://www.recipetineats.com/tachyon/2020/10/Goan-Fish-Curry_5.jpg"
 },
@@ -260,14 +260,14 @@ export const preguntas = [
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/jheengMS3.jpg"
 },
 {
-  pregunta: "Jheenga Punjabi 🦐🍅🧅",
+  pregunta: "Jheenga Punjabi 🥣🦐🍅🧅",
   respuesta: "Afrodisiaco camarones estilo indio salsa de tomate cebolla y masala con aliño kasturi methi",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/JheengaP2.jpg"
 },
 {
-  pregunta: "Jheenga Tawa Masala 🦐🧅",
+  pregunta: "Jheenga Tawa Masala 🍤🧅",
   respuesta: "Camaron pimenton, cebolla y masala",
-  imagen: "https://www.google.com/url?sa=i&url=https%3A%2F%2Famara.qa%2Fmenu%2Fseafood-curry%2Fjhinga-tawa-masala&psig=AOvVaw1YTb477WfgcOZSS-v0zXDf&ust=1746536783980000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCMj81tmyjI0DFQAAAAAdAAAAABAE"
+  imagen: "https://amara.qa/uploads/menu-item/image/2023/03/jhinga-tawa-masala-323.png"
 },
 {
   pregunta: "❤️ Balti Gosh 🗑️🐑🍖🍅🧅🌿",
@@ -275,22 +275,17 @@ export const preguntas = [
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/Balty_Ghost2.jpg"
 },
 {
-  pregunta: "Mathan Lamb Chop 🕉️🐑🍖",
+  pregunta: "Mathan Lamb Chop 🗑️🕉️🐑🍖",
   respuesta: "Costilla francesa de cordero verdura sobre suave curry",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/Balty_Ghost2.jpg"
 },
 {
-  pregunta: "Crispy Panner Sandwich 🍔🥬🍅🧅",
-  respuesta: "Panner crujiente, lechuga, tomate, cebolla, mayo y chutney de menta",
-  imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/Balty_Ghost2.jpg"
-},
-{
-  pregunta: "Crispy Panner Sandwich 🍔🥬🍅🧅",
+  pregunta: "Crispy Panner Sandwich 🍔🧀🥬🍅🧅",
   respuesta: "Panner crujiente, lechuga, tomate, cebolla, mayo y chutney de menta",
   imagen: "https://rishtedar.cl/wp-content/uploads/2025/01/pannerTikka_web.png"
 },
 {
-  pregunta: "Ensalada Jheenga Til Tikka 🦐🥗🍍",
+  pregunta: "Ensalada Jheenga Til Tikka 🍤🥗🍍",
   respuesta: "Mix verdes camaron apanado con sesamo y piña asada",
   imagen: "/platos/Ensalada_Jheenga_Til_Tikka.png"
 },
@@ -300,7 +295,7 @@ export const preguntas = [
   imagen: "https://rishtedar.cl/wp-content/uploads/2024/10/benganS-1.png"
 },
 {
-  pregunta: "Murgh Tikka Italiano Sandwich 🍗🥑🍅",
+  pregunta: "Murgh Tikka Italiano Sandwich 🍔🐓🥑🍅",
   respuesta: "Pollo murgh tikka, tomate, ollo, mayo",
   imagen: "https://rishtedar.cl/wp-content/uploads/2024/10/murghts.png"
 },
@@ -320,7 +315,7 @@ export const preguntas = [
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/naan.jpg"
 },
 {
-  pregunta: "Tandoori Kulcha 🍞🧅",
+  pregunta: "Tandoori Kulcha 🍞🧅🍃",
   respuesta: "Pan con Cebolla y Cilantro",
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/TKulcha3.jpg"
 },
@@ -340,14 +335,36 @@ export const preguntas = [
   imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/nimboB.jpg"
 },
 {
-  pregunta: "Murgh Pacora 🍗",
+  pregunta: "Murgh Pacora 👧🧑🐓",
   respuesta: "Nuggets de pollo apanados con masa de garbanzo",
-  imagen: "https://rishtedar.cl/wp-content/uploads/2023/08/nimboB.jpg"
+  imagen: "https://cdn3.didevelop.com/public/cdn/533_f9fa93c9f40ba4981a70fab3b54234ba.jpg"
 },
 {
-  pregunta: "Chicken Pacora Sandwich 🍔🍗🧀🥬🍅🍟",
+  pregunta: "Chicken Pacora Sandwich 👧🧑🍔🐓🧀🥬🍅🍟",
   respuesta: "Mini sándwich de pollo apanado en harina de garbanzo, ketchup, queso laminado, tomate, lechuga. Incluye porción papas fritas.",
   imagen: ""
+},
+
+// Niveles de Picante
+{
+  pregunta: "Namaste Heat 🌿",
+  respuesta: "Un toque suave y aromático",
+  imagen: "/Spicy-01.png"
+},
+{
+  pregunta: "Bollywood Kick 🌶️",
+  respuesta: "Sabor con carácter",
+  imagen: "/Spicy-02.png"
+},
+{
+  pregunta: "Rajah Flame  🔥",
+  respuesta: "Intenso como un festival de especias",
+  imagen: "/Spicy-03.png"
+},
+{
+  pregunta: "Shiva’s Fury – ☢️",
+  respuesta: "Solo para valientes",
+  imagen: "/Spicy-04.png"
 },
 // BEBIDAS
 // {
